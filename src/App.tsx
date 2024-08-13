11168aba98e092c9f678db4687427f333c592262
Pedro Henrique Admin <doctorfast10.12@hotmail.com>
2024-08-13 13:59:11 -0300

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Login } from './pages/login/login'
+import { Login } from './pages/login/Login'
 import { Register } from './pages/register/Register'
 import { Home } from './pages/home/Home'
 
