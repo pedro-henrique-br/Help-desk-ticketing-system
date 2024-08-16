0c68aa4f69f4a9699751f9b27b4144f54f819979
Pedro Henrique Admin <doctorfast10.12@hotmail.com>
2024-08-16 14:46:46 -0300

    fix: error painel

--- src/parts/aside/Aside.tsx
+++ src/parts/aside/Aside.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { Dashboard } from "../../pages/Dashboard/Dashboard";
 
@@ -23,7 +22,7 @@ function TabPanel(props: TabPanelProps) {
       {...other}>
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          {children}
         </Box>
       )}
     </div>
