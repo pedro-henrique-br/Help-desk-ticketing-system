ec4040dc6e28cad39655727bcf03c6dac6807f95
Pedro Henrique Admin <doctorfast10.12@hotmail.com>
2024-09-03 10:51:21 -0300

    chore: userSettings

--- src/pages/userSettings/UserSettings.tsx
+++ src/pages/userSettings/UserSettings.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { motion } from "framer-motion";
 import { Navigate } from "react-router-dom";
+import { Bounce, toast } from "react-toastify";
+import { supabaseClient } from "../../services/supabase";
 
 interface userData {
   name: string;
@@ -17,16 +19,55 @@ export const UserSettings = () => {
   const [user, setUser] = useState<userData | never>();
   const [handleEdit, setHandleEdit] = useState(Boolean(true));
   const [isButtonPress, setIsButtonPress] = useState(Boolean(false));
+  const [userName, setUserName] = useState("")
+  const [userEmail, setUserEmail] = useState("")
+  const [userRamal, setUserRamal] = useState("")
+
+  const handleSubmit = () => {
+    if((userName === "" || userEmail === "") || userRamal === ""){
+      toast.info(`Suas informações não podem ser nulas!`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    } else {
+      const userChange = {
+        name: userName,
+        email: userEmail,
+        ramal: userRamal,
+      }
+      api.changeUserInfo(userChange)
+    }
+  }
+
+  const fetchUser = async () => {
+    const data = await api.getUserInfo();
+    if (data) {
+      setUser(data[0]);
+      setUserName(data[0]?.name)
+      setUserEmail(data[0]?.email)
+      setUserRamal(data[0]?.ramal)
+    }
+  };
 
   useEffect(() => {
-    const fetchUserName = async () => {
-      const data = await api.getUserInfo();
-      if (data) {
-        setUser(data[0]);
-      }
-    };
-    fetchUserName();
+    fetchUser();
   }, []);
+
+  supabaseClient.supabase
+    .channel("users")
+    .on(
+      "postgres_changes",
+      { event: "UPDATE", schema: "public", table: "users" },
+      fetchUser
+    )
+    .subscribe();
 
   return (
     <>
@@ -47,14 +88,13 @@ export const UserSettings = () => {
                 flexDirection: "column",
                 alignItems: "center",
                 width: "100vw",
-                height: "100vh",
+                height: "80vh",
               }}>
               <Box
                 sx={{
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
-                  mb: "30vh",
                   gap: "100px",
                   width: "100vw",
                 }}>
@@ -63,20 +103,23 @@ export const UserSettings = () => {
                   <Typography fontSize={"2rem"}>{user?.name}</Typography>
                   <InputLabel>Nome</InputLabel>
                   <TextField
-                    sx={{ mb: "15px" }}
-                    value={user?.name}
+                    sx={{ mb: "15px", width: "300px" }}
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
                     disabled={handleEdit}
                     placeholder={"Nome"}></TextField>
                   <InputLabel>Email</InputLabel>
                   <TextField
                     sx={{ mb: "15px", width: "300px" }}
-                    value={user?.email}
+                    value={userEmail}
+                    onChange={(e) => setUserEmail(e.target.value)}
                     disabled={handleEdit}
                     placeholder={"Email"}></TextField>
                   <InputLabel>Ramal</InputLabel>
                   <TextField
-                    sx={{ mb: "15px" }}
-                    value={user?.ramal}
+                    sx={{ mb: "15px", width: "300px" }}
+                    value={userRamal}
+                    onChange={(e) => setUserRamal(e.target.value)}
                     disabled={handleEdit}
                     placeholder={"Ramal"}></TextField>
                   <Typography fontSize={"1rem"}>Tipo de usuário</Typography>
@@ -86,15 +129,21 @@ export const UserSettings = () => {
                       user?.isAdmin ? "Administrador" : "Usuario Comum"
                     }>
                   </TextField>
-                 
-                </Box>
-                
-              </Box>  
+                  <Box sx={{mt: 3, display: "flex", justifyContent: "space-around", width: "300px" }}>
                   <Button
-                  sx={{mb: "25vh", position: "absolute", bottom: 0}}
+                  sx={{}}
                     onClick={() => setHandleEdit(handleEdit ? false : true)}>
                     Editar usuário
                   </Button>
+                  <Button
+                  variant="contained"
+                  sx={{}}
+                    onClick={handleSubmit}>
+                    Salvar
+                  </Button>
+                  </Box>
+                </Box>
+              </Box>  
              
             </Box> <Button
                 variant="contained"
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import { Bounce, toast } from "react-toastify";
 import { supabaseClient } from "./supabase";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
 interface ticket {
   request_type: string;
@@ -11,6 +11,12 @@ interface ticket {
   created_at: string;
   status: string;
   assignee: string;
+}
+
+interface userData {
+  name: string;
+  email: string;
+  ramal: string;
 }
 
 const createTicket = async (
@@ -26,7 +32,7 @@ const createTicket = async (
     .select("*")
     .eq("user_id", userId);
 
-  if (data){
+  if (data) {
     const response = await supabaseClient.supabase.from("tickets").insert({
       user_id: userId,
       user_name: data[0].name,
@@ -52,8 +58,8 @@ const createTicket = async (
         transition: Bounce,
       });
       setTimeout(() => {
-        window.location.href = "/home"
-      }, 2000)
+        window.location.href = "/home";
+      }, 2000);
     } else {
       toast.error(`Ocorreu um erro ${response.error?.message}`, {
         position: "top-right",
@@ -82,16 +88,23 @@ const getUserTickets = async (userId: string) => {
 };
 
 const getUserByName = async (name: string) => {
-  const { data } = await supabaseClient.supabase.from("users").select("*").textSearch("name", name).select()
-  return data
-}
+  const { data } = await supabaseClient.supabase
+    .from("users")
+    .select("*")
+    .textSearch("name", name)
+    .select();
+  return data;
+};
 
 const getUserInfo = async () => {
-  const userId = Cookies.get("user_id")
-  const { data, error } = await supabaseClient.supabase.from("users").select("*").eq("user_id", userId);
-  if(data){
-    return data
-  } else if(error){
+  const userId = Cookies.get("user_id");
+  const { data, error } = await supabaseClient.supabase
+    .from("users")
+    .select("*")
+    .eq("user_id", userId);
+  if (data) {
+    return data;
+  } else if (error) {
     toast.error(`Ocorreu um erro ${error?.message}`, {
       position: "top-right",
       autoClose: 5000,
@@ -104,7 +117,7 @@ const getUserInfo = async () => {
       transition: Bounce,
     });
   }
-}
+};
 
 const getAllTickets = async () => {
   const { data } = await supabaseClient.supabase.from("tickets").select("*");
@@ -114,7 +127,9 @@ const getAllTickets = async () => {
 };
 
 const uploadFile = async (file: File) => {
-  const { error } = await supabaseClient.supabase.storage.from('screenshots').upload(`${file.name}`, file)
+  const { error } = await supabaseClient.supabase.storage
+    .from("screenshots")
+    .upload(`${file.name}`, file);
   if (error) {
     toast.error(`Ocorreu um erro com o arquivo inserido ${error.message}`, {
       position: "top-right",
@@ -127,11 +142,15 @@ const uploadFile = async (file: File) => {
       theme: "light",
       transition: Bounce,
     });
-  } 
-}
+  }
+};
 
 const changeAssignee = async (id: number, name: string) => {
-  const { data, error } = await supabaseClient.supabase.from("tickets").update({"assignee": name}).eq("id", id).select();
+  const { data, error } = await supabaseClient.supabase
+    .from("tickets")
+    .update({ assignee: name })
+    .eq("id", id)
+    .select();
   if (error) {
     toast.error(`Ocorreu um erro ao atender o chamado ${error.message}`, {
       position: "top-right",
@@ -144,7 +163,7 @@ const changeAssignee = async (id: number, name: string) => {
       theme: "light",
       transition: Bounce,
     });
-  } else if (data){
+  } else if (data) {
     toast.info(`Chamado sendo atendido`, {
       position: "top-right",
       autoClose: 5000,
@@ -156,12 +175,15 @@ const changeAssignee = async (id: number, name: string) => {
       theme: "light",
       transition: Bounce,
     });
-    }
-}
+  }
+};
 
 const closeTicket = async (ticketId: number) => {
-  const response = await supabaseClient.supabase.from("tickets").delete().eq("id", ticketId)
-  if(response?.error){
+  const response = await supabaseClient.supabase
+    .from("tickets")
+    .delete()
+    .eq("id", ticketId);
+  if (response?.error) {
     toast.error(`Ocorreu um erro ao fechar o chamado ${response?.error}`, {
       position: "top-right",
       autoClose: 5000,
@@ -173,7 +195,7 @@ const closeTicket = async (ticketId: number) => {
       theme: "light",
       transition: Bounce,
     });
-  } else if(response?.status === 204) {
+  } else if (response?.status === 204) {
     toast.info(`Chamado fechado`, {
       position: "top-right",
       autoClose: 5000,
@@ -185,9 +207,28 @@ const closeTicket = async (ticketId: number) => {
       theme: "light",
       transition: Bounce,
     });
-    return response
+    return response;
   }
-}
+};
+
+const changeUserInfo = async (user: userData) => {
+  const userId = Cookies.get("user_id");
+  await supabaseClient.supabase
+    .from("users")
+    .update({ email: user?.email })
+    .eq("user_id", userId);
+  await supabaseClient.supabase.auth.updateUser({
+    email: user.email,
+  });
+  await supabaseClient.supabase
+    .from("users")
+    .update({ name: user?.name })
+    .eq("user_id", userId);
+  await supabaseClient.supabase
+    .from("users")
+    .update({ ramal: user?.ramal })
+    .eq("user_id", userId);
+};
 
 export const api = {
   createTicket,
@@ -197,5 +238,6 @@ export const api = {
   getUserInfo,
   changeAssignee,
   closeTicket,
-  getUserByName
+  getUserByName,
+  changeUserInfo,
 };
